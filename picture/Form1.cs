36e8a9bb56dc6440642e2f1fe5fc292f1bd86e73
tetraxy <ksusha.sh@list.ru>
2025-02-17 21:47:56 +0800

    Версия 1.0
    Реализован поиск случайной фотографии по запросу пользователя

--- picture/Form1.cs
+++ picture/Form1.cs
@@ -7,18 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
 
 namespace picture
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
+            client.DefaultRequestHeaders.Add("X-Api-Key", "I8Y8O9NbvXQ4wqmsgrze7g==tJWrfgtuOLmJII5s");
+            client.DefaultRequestHeaders.Add("Accept", "image/jpg");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string category = "nature"; // Вы можете позволить пользователю выбрать категорию
+            string apiUrl = $"https://api.api-ninjas.com/v1/randomimage?category={category}";
+
+            try
+            {
+                var response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        // Загружаем изображение в PictureBox
+                        using (var image = Image.FromStream(stream))
+                        {
+                            pictureBox1.Image = new Bitmap(image);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}");
+            }
 
         }
     }
---
+++ picture/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new picture.Form1()); // Запускаем Form1
+        }
+    }
+
